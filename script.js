8662101110923893038d8140d90c88bc1159c6fb
Greg Marmolejo <greg_marmolejo@yahoo.com>
2019-12-20 08:07:11 -0600

    added datePickerChange, handleSave, setCurrentDateLabel, loadDay, checkTimeBlocks, createTimeBlock, createEL, getTense, getStoreDatePrefix.

--- script.js
+++ script.js
@@ -47,4 +47,147 @@ function multiDayChecked () {
     }
 }
 
-// Event Listeners  (Save buttons) - all of these are defined in functions.
+// If a new date was selected from the Date Picker
+function datePickerChange() {
+    //Get the new date - If the date is not valid, default to today
+    curDate = moment($("#datepicker").val(), "YYYY-MM-DD");
+    if (!curDate.isValid()) {
+        curDate = moment();
+    }
+    //Update the header, cancel existing timers, fade in the content 
+    setCurrentDateLabel();
+    loadDay(fadeNormal);
+}
+
+// Event Listener  (Save buttons).
+function handleSave() {
+    var $desc = $(this).siblings(".description");
+    var hour = $desc.attr("data-hour");
+    var text = $desc.val();
+    localStorage.setItem(getStoreDatePrefix() + hour.trim(), text.trim());
+    $("#save-toast").fadeIn(fadeToastIn).fadeOut(fadeToastOut);
+}
+
+// Sets the current day in the header
+function setCurrentDateLabel () {
+    $("#currentDay").text(curDate.format('dddd, MMMM, Do'));
+}
+
+// Load the current day onto the page
+function loadDay(fadeTime=0) {
+    clearInterval(updateinterval)
+
+    $(".container").html(""); // Clear out data
+    // Create and Load Time Blocks
+    for(var index=0; index<times.length; index++) {
+        $(".container").append(createTimeBlock(times[index]));
+    }
+
+/// Setup interval to update past, present, and future classes every 30s.
+    updateinterval = setInterval(checkTimeBlocks, timeBlockDelayMS);
+
+    /////////// Styling ////////////////
+
+    // Change opacity of description in hover
+    $('description').hover( function() {
+        $(this).toggleClass("active");
+    });
+    
+    //Hover over save button changes opacity and makes disk larger//
+    $('.saveBtn').hover( function() {
+        $(this).toggleClass("active");
+    });
+
+    $(".container").hide().fadeIn(fadeTime);
+}
+
+// Checks if the timeblocks' tense has changed
+function checkTimeBlocks() {
+    console.log("Check Time Block Active");
+    var $descriptions = $('.description');
+    $descriptions.each(function(index) {
+        var hour12 = $(this).attr("data-hour"); // Get hour
+        var t = getMoment12H(hour12);
+        var tense = getTense(t)
+        if ($(this).hasClass(tense)) {
+            //console.log("/NO CHANGE");
+        } else if (tense === "present") {
+            $(this).removeClass("past future");
+        } else if (tense === "past") {
+            $(this).removeClass("present future");
+        } else if (tense === "future") {
+            $(this).removeClass("present past");
+        } else {
+            alert("Unknown Tense");
+        }
+        $(this).addClass(tense);
+    });
+}
+
+/// Create a time block group
+function createTimeBlock(hour24) {
+    var row = createEl("div", "row");
+    var timeBlock = createEl("div", "time-block");
+    timeBlock.appendChild(row);
+    var colHour = createEl("div", "col-sm-1 col-12 pt-3 hour", hour24);
+    row.appendChild(colHour);
+    var colText = createEl("textarea", "col-sm-10 col-12 description", hour24);
+    row.appendChild(colText);
+    var colSave = createEl("div", "col-sm-1 col-12 saveBtn");
+    row.appendChild(colSave);
+    var icon = createEl("i", "fas fa-save");
+    colsave.appendChild(icon);
+
+    return timeBlock;
+}   
+// Create a single page element
+// tag = tag to create
+// cls = classes to assign
+// hour24 = the current hour (only used by hour and description classes)
+function createEL(tag, cls, hour24) {
+    var el= document.createElement(tag);
+    // Special Handling for Hour and Description Columns which need the hour.
+    if (hour24) {
+        var t = getMoment24H(hour24);
+        let displayHour = formatAmPm(t);
+        if (cls.includes("description")) {
+            // description class
+            cls +=" " + getTense(t);
+            el.textContent = localStorage.getItem(getStoreDatePrefix() + displayHour);
+            el.setAttribute("data-hour", displayHour);
+        } else {
+            // hour class
+            el.textContent = displayHour.padEnd(4, " ");
+        }   
+    }  
+    // Set the class of the element
+    el.setAttribute("class", cls);
+    return el; 
+}
+
+// Check to see if the specified time is in the past, present, or future, compared to current time.
+function getTense(t) {
+    var cls;
+    var n = moment();
+    
+    if (n.isSame(t, "hour") &&
+        n.isSame(t, "day")  &&
+        n.isSame(t, "month") &&
+        n.isSame(t, "year")) {
+            cls = "present";
+        } else if (n.isAfter(t)) {
+            cls = "past"
+        } else {
+            cls = "future";
+        }
+        return cls;
+}
+
+/// Get String
+
+// Get string prefix for localStorage based off curDate
+function getStoreDatePrefix() {
+    return curDate.format("YYYYMMDD-");
+}
+
+// Return the moment formatted as a 12-hour AM/PM time string (Example: 10AM)
